275ac35976f89651ad4ed4a8a84d74dd2b8118d9
Thor Tomasarson <thor.tomasarson@marel.com>
2024-09-12 15:49:26 +0000

    Avoiding build issues

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,2 @@
-export let assistantId = "asst_15DzgalkRpZFYO9uc7IEvHTo"; // "asst_dj8iKIlbrCxwBsNzUqNkps4e"; // set your assistant ID here
-
-if (assistantId === "") {
-  assistantId = process.env.OPENAI_ASSISTANT_ID;
-}
+// Provide a default value if the environment variable is not set
+export let assistantId: string = process.env.OPENAI_ASSISTANT_ID || 'asst_15DzgalkRpZFYO9uc7IEvHTo';
